86e65be2850b324c941480096e74a295369ebd0c
JavRomero89 <javieromero23@gmail.com>
2023-06-22 17:46:48 +0200

    Finish

---
+++ variables.js
@@ -0,0 +1,66 @@
+let name = "Javier"
+let age = 34
+let city = "Lima"
+let isMarried = true
+let year = 2023
+
+console.log(name);
+console.log(age);
+console.log(city);
+console.log(isMarried);
+console.log(year);
+console.log("--------------------");
+
+let num1 = 4 > 3
+let operation = 20 <= 25
+let num = 4 ** 2 >= 2 ** 3
+let age1 = 7 === "7"
+let operation1 = 2 ** 3 + 2 >= 5 ** 2
+let tiburoncin = 2 > 1 && 3 < 2
+
+
+console.log(num1);
+console.log(operation);
+console.log(num);
+console.log(age1);
+console.log(operation1);
+console.log(tiburoncin);
+console.log("--------------------");
+
+let side = 4
+let squareArea = side ** 2
+
+console.log(squareArea);
+console.log("--------------------");
+
+let a = 2
+let b = 3
+let c = 4
+let trianglePerimeter = a+b+c
+
+console.log(trianglePerimeter);
+console.log("--------------------");
+
+let radius = 7
+let PI = Math.PI
+let circleArea = PI * radius**2
+
+console.log(circleArea);
+console.log("--------------------");
+
+let circumference = 2 * Math.PI * radius
+
+console.log(circumference);
+console.log("--------------------");
+
+let num2 = 5
+num2+= 10
+console.log(num2);
+console.log("--------------------");
+
+num2 -=2
+
+console.log(num2);
+
+
+
